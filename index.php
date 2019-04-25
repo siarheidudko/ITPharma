97e05ad0399073e855e5bcbb0a21370c2c9c82d2
root <root@itpharma.by>
2019-04-25 06:01:51 -0400

    upd

--- index.php
+++ index.php
@@ -12,8 +12,10 @@
 
 <?php get_header(); ?>
 
-<?php
-	echo apply_filters('the_content', get_post()->post_content);
-?>
+<div id="bodycontent">
+	<?php
+		echo apply_filters('the_content', get_post()->post_content);
+	?>
+</div>
 
 <?php get_footer(); ?>
